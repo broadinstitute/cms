c77b64c0d66324e16d46a4311d1d805ab8bdf8eb
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-04-13 18:39:03 -0400

    changed doc theme from RTD to default
    ironically readthedocs.com cannot ingest docs prepared using their own
    Sphinx theme, so it is now changed to the default theme which should
    work

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -129,12 +129,10 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'default'
+#import sphinx_rtd_theme
+#html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
