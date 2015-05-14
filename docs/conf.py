c1b0f981ceb23e977f61d45113113baa2c0a35ff
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-05-14 17:09:44 -0400

    fixed sphinx-argparse relative directory

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.dirname(os.path.abspath('.')))
+print "PATH: {}".format(os.path.dirname(os.path.abspath('../cms/cms/')))
+sys.path.insert(0, os.path.dirname(os.path.abspath('../cms/cms/')))
 
 # -- Mock out the heavyweight pip packages, esp those that require C ----
 import mock
