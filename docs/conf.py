6837f0f44c83969cb527823f4c5fbf04de4180f9
Chris Tomkins-Tinch <tomkinsc@broadinstitute.org>
2015-05-14 16:38:33 -0400

    added sphinxarg.ext to document extensions

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.pngmath',
     'sphinx.ext.viewcode',
+    'sphinxarg.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
