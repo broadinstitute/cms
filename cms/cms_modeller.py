37a946cb3a8ed16fbe8d95f7658c5fdd468a7534
josephvitti <vitti@broadinstitute.org>
2016-06-27 14:41:41 -0400

    JV test docs 38

--- cms/cms_modeller.py
+++ cms/cms_modeller.py
@@ -16,7 +16,7 @@ def parser_bootstrap(parser):
 	if parser == None:
 		parser=argparse.ArgumentParser(description="exploratory fitting of demographic models to population genetic data as part of CMS 2.0 package.\nSource code and binaries can be found at https://github.com/broadinstitute/cms>")
 	#parser.help = '''test test'''
-	parser.add_argument('subcommand', type=str, default="bootstrap", help="also a test")
+	#parser.add_argument('subcommand', type=str, default="bootstrap", help="also a test")
 	parser.add_argument('-tped',type=str,default=None)
 	parser.add_argument('-recom',type=str,default=None)
 	parser.add_argument('-regions',type=str,default=None)
