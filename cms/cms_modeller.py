7e3d85cc8592ad34296087a485577cadfb9b6ce3
josephvitti <vitti@broadinstitute.org>
2016-06-27 15:17:08 -0400

    JV docs test 48

--- cms/cms_modeller.py
+++ cms/cms_modeller.py
@@ -11,10 +11,14 @@ __commands__ = []
 
 
 def parser_bootstrap():
-	parser=argparse.ArgumentParser(description="exploratory fitting of demographic models to population genetic data", help="test foobar apricots")
+	parser=argparse.ArgumentParser(description="exploratory fitting of demographic models to population genetic data")
 	#parser.help = '''test test'''
 	parser.add_argument('subcommand', type=str, default="bootstrap", help="this is required")
 	parser.add_argument('-tped',type=str,default=None)
+
+	subparsers = parser.add_subparsers()
+	subparser.add_argument('ref', type=str, help='foo1 help')
+
 	parser.add_argument('-recom',type=str,default=None)
 	parser.add_argument('-regions',type=str,default=None)
 	parser.add_argument('-out',type=str,default=None)
