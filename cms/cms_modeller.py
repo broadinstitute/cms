0d2c930141f4f6b6a9db85576ef471e8100df0a7
josephvitti <vitti@broadinstitute.org>
2016-06-29 10:08:59 -0400

    cms_modeller docs

--- cms/cms_modeller.py
+++ cms/cms_modeller.py
@@ -1,5 +1,6 @@
-## top-level script for demographic modeling as part of CMS 2.0. (assumes C programs have been compiled in same directory)
-## last updated: 06.28.16 vitti@broadinstitute.org
+## top-level script for demographic modeling as part of CMS 2.0. 
+#(CURRENTLY: assumes C programs have been compiled in same directory -- JV must provide Makefile; assumes python in $PATH)
+## last updated: 06.29.16 vitti@broadinstitute.org
 
 import subprocess
 import argparse
@@ -16,14 +17,20 @@ def full_parser_cms_modeller():
 	## CALCULATE TARGET ##
 	######################
 	target_stats_parser = subparsers.add_parser('target_stats', help='perform per-site(/per-site-pair) calculations of population summary statistics for model target values')
-	target_stats_parser.add_argument('tpeds', action='store', help='comma-delimited list of input tped files (i.e., for all populations to jointly model)',type=list)
+	target_stats_parser.add_argument('tpeds', action='store', help='comma-delimited list of input tped files (only one file per pop being modelled; must run chroms separately or concatenate)',type=list)
 	target_stats_parser.add_argument('recom', action='store', help='recombination map') #check consistent with used to create tped
-	target_stats_parser.add_argument('regions', action='store', help='putative neutral regions') #make this optional and/or TSV
+	target_stats_parser.add_argument('--regions', action='store', help='putative neutral regions') #make this optional and/or TSV
+	target_stats_parser.add_argument('--freqs', action='store_true', help='calculate summary statistics from within-population allele frequencies') 
+	target_stats_parser.add_argument('--ld', action='store_true', help='calculate summary statistics from within-population linkage disequilibrium') 
+	target_stats_parser.add_argument('--fst', action='store_true', help='calculate summary statistics from population comparison using allele frequencies') 
 	target_stats_parser.add_argument('out', action='store', help='outfile prefix') 
 	
 	bootstrap_parser = subparsers.add_parser('bootstrap', help='perform bootstrap estimates of population summary statistics in order to finalize model target values')
 	bootstrap_parser.add_argument('n', action='store', type=int, help='number of bootstraps to perform in order to estimate standard error of the dataset (should converge for reasonably small n)')
-	bootstrap_parser.add_argument('ins', action='store', help='comma-delimited list of infiles with per-site(/per-site-pair) calculations. For bootstrap estimates of genome-wide values, should first concatenate per-chrom files)') 
+
+	bootstrap_parser.add_argument('--in-freqs', action='store', help='comma-delimited list of infiles with per-site calculations for population. For bootstrap estimates of genome-wide values, should first concatenate per-chrom files)') 
+	bootstrap_parser.add_argument('--in-ld', action='store', help='comma-delimited list of infiles with per-site-pair calculations for population. For bootstrap estimates of genome-wide values, should first concatenate per-chrom files)') 
+	bootstrap_parser.add_argument('--in-fst', action='store', help='comma-delimited list of infiles with per-site calculations for population pair. For bootstrap estimates of genome-wide values, should first concatenate per-chrom files)') 	
 	bootstrap_parser.add_argument('out', action='store', help='outfile prefix') 
 	
 	######################
@@ -52,6 +59,7 @@ def full_parser_cms_modeller():
 ## AUX. FUNCTIONS ###
 ######################
 def execute_target_stats(args):
+	'''calls bootstrap_*_popstats_regions to get per-snp/per-snp-pair values; these programs currently have hard-coded arg input -- JV consider switching to getopt'''
 	inputtpedstring = ''.join(args.tpeds)
 	inputtpeds = inputtpedstring.split(',')
 	npops = len(inputtpeds)
@@ -59,30 +67,42 @@ def execute_target_stats(args):
 	allCmds = []
 	for ipop in range(npops):
 		inputtped = inputtpeds[ipop]
-		freqCmd = ['bootstrap_freq_popstats_regions ', inputtped, args.recom, args.regions, args.out + "_freqs_" + str(ipop)]
-		ldCmd = ['bootstrap_ld_popstats_regions ', inputtped, args.recom, args.regions, args.out + "_ld_" + str(ipop)]
-		allCmds.extend([freqCmd, ldCmd])
-		for jpop in range(ipop+1, npops):
-			inputtped2 = inputtpeds[jpop]
-			fstCmd = ['bootstrap_fst_popstats_regions ', inputtped, inputtped2, args.recom, args.regions, args.out + "_fst_" + str(ipop) + "_" + str(jpop)]
-			allCmds.append(fstCmd)
-	#print allCmds
+		if args.freqs:
+			freqCmd = ['bootstrap_freq_popstats_regions ', inputtped, args.recom, args.regions, args.out + "_freqs_" + str(ipop)]
+			allCmds.append(freqCmd)
+		if args.ld:
+			ldCmd = ['bootstrap_ld_popstats_regions ', inputtped, args.recom, args.regions, args.out + "_ld_" + str(ipop)]
+			allCmds.append(ldCmd)
+		if args.fst:
+			for jpop in range(ipop+1, npops):
+				inputtped2 = inputtpeds[jpop]
+				fstCmd = ['bootstrap_fst_popstats_regions ', inputtped, inputtped2, args.recom, args.regions, args.out + "_fst_" + str(ipop) + "_" + str(jpop)]
+				allCmds.append(fstCmd)
 	for command in allCmds:
 		command = [str(x) for x in command]
-		subprocess.check_call( command )
+		#subprocess.check_call( command )
+		print command
 	return
 def execute_bootstrap(args):
+	'''pulls all per-snp/per-snp-pair values to get genome-wide bootstrap estimates. adapted from JV experimental: get_neutral_targetstats_from_bootstrap.py'''
 	inputestimatefilenames = ''.join(args.ins)
 	inputfilenames = inputestimatefilenames.split(',')
 	npops = len(inputfilenames)
 	print "estimating bootstrap values of summary statistics for " + str(npops) + " populations..."
+	allCmds = []
+	for ipop in range(npops):
+		inputfilename = inputfilenames[ipop]
+		bootstrapCmd = 'python get_neutral_targetstats_from_bootstrap.py '
+
 	print "MUST CONNECT PIPELINE from cms_modeller.py TO get_neutral_targetstats_from_bootstrap.py"
 	return
 def execute_point(args):
+	'''runs simulates of a point in parameter-space, comparing to specified target. adapted from JV experimental: grid_point.py'''
 	print "generating " + str(n) + " simulations from model: " + args.modelfile
 	print "MUST CONNECT PIPELINE from cms_modeller.py TO grid_point.py"
 	return
 def execute_grid(args):
+	''' '''
 	print "loading dimensions of grid to search from: " + args.grid_inputdimensionsfile
 	print "MUST CONNECT PIPELINE from cms_modeller.py TO grid.py"
 	return
