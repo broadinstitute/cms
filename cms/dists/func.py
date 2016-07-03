e3c1a3f46c864630ca1d275f2cbcac8b2c669253
josephvitti <vitti@broadinstitute.org>
2016-07-03 15:58:36 -0400

    intermediate commit

--- cms/dists/func.py
+++ cms/dists/func.py
@@ -5,6 +5,9 @@ import subprocess
 import sys
 import os
 
+##################
+### BOOKKEEPING ##
+##################
 def give_command(commandstr):
 	commands = commandstr.split(' ')
 	subprocess.check_output(commands)
@@ -41,6 +44,10 @@ def check_file_len(filename):
 		return iline
 	else:
 		return 0
+
+###################
+### SELFREQ BINS ##
+###################
 def get_bin_strings(bin_medians):
 	'''given input bin medians, returns minimal len strs needed to create unique bin labels'''
 	stringlen = 3 #minimum is '0.x' -- not necessarily the most descriptive way to round these, but it's functional.
@@ -68,3 +75,197 @@ def check_bin_filled(directory, numsims):
 		return False
 	else:
 		return True
+
+#####################
+## CALCS FROM SIMS ## ## gratuitous. should probably nix (?)
+#####################
+def calc_ihs(inputTped, outputFile, runProgram = "scans.py", numThreads = 7):
+	'''from func_clean.py'''
+	cmdStr = "python " + runProgram + " selscan_ihs " + inputTped + " " + outputFile + " --threads " + str(numThreads)
+	print cmdStr
+	return
+def calc_delihh(readfilename, writefilename):
+	"""given a selscan iHS file, parses it and writes an analogous file with delIHH information"""
+	readfile = open(readfilename, 'r')
+	writefile = open(writefilename, 'w')
+	for line in readfile:
+		entries = line.split()
+		locus, phys, freq_1, ihh_1, ihh_0, ihs, ihh_0_l, ihh_0_r, ihh_1_l, ihh_1_r = entries #[0:10]
+					#ancestral - derived
+		unstand_delIHH = fabs(float(ihh_1) - float(ihh_0))
+		writeline = locus + "\t" + phys + "\t" + freq_1 + "\t" + str(ihs) + "\t" + str(unstand_delIHH) +"\t" + str(unstand_delIHH) +  "\n" #6 columns for selscan norm
+		writefile.write(writeline)
+	writefile.close()
+	readfile.close()
+def calc_xp(inputTped, inputTped2, outputFile, runProgram = "scans.py", numThreads = 7):
+	'''from func_clean.py'''
+	cmdStr = "python " + runProgram + " selscan_xpehh " + inputTped + " " + outputFile + " " + inputTped2 + " --threads " + str(numThreads)
+	print cmdStr
+	return	
+def calc_fst_deldaf(inputTped, inputTped2, outputFile):
+	print "assumes C program compiled "
+	commandstring = "./calc_fst_deldaf_tped"
+	argstring = inputTped1 + " " + inputTped2 + " " + outputFile
+	print commandstring + " " + argstring
+	return
+def read_neut_normfile(neutNormfilename, scoretype ='ihs'):
+	"""pulls means and vars so that we can normalize sel scenarios with the same parameters. pulled from func_clean.py"""
+	openfile = open(neutNormfilename)
+	line = openfile.readline()
+	entries = line.split()
+	while len(entries) < 1 or entries[0] != "Total":
+		line = openfile.readline()
+		entries = line.split()
+	nsnp = int(entries[-1])
+	while "num" not in entries:
+		line = openfile.readline()
+		entries = line.split()
+	if scoretype == "ihs":
+		assert entries[0] == "bin"
+		bins, nums, means, variances = [], [], [], []
+		for line in openfile:
+			entries = line.split()
+			if entries[0] != "Normalizing":
+				binlimit, numinbin, mean, variance = float(entries[0]), int(entries[1]), float(entries[2]), float(entries[3])
+				bins.append(binlimit)
+				nums.append(numinbin)
+				means.append(mean)
+				variances.append(variance)
+		openfile.close()
+		return bins, nums, means, variances
+	elif scoretype == "xp":
+		dataline = openfile.readline()
+		entries = dataline.split()
+		num, mean, var = int(entries[0]), float(entries[1]), float(entries[2])
+		#print str(num)
+		#print str(nsnp)
+		#assert num == nsnp
+		return num, mean, var
+	openfile.close()
+	return
+def norm_neut_ihs(inputScoreFile, outfileName, runProgram = "scans.py"):
+	'''from func_clean.py'''
+	cmdStr = "python " + runProgram + " selscan_norm_ihs " + inputScoreFile + " > " + outfileName
+	print cmdStr
+	return
+def norm_sel_ihs(inputScoreFile, neutNormfilename, bin_bounds):
+	''' from normalize_Ihs_manually() in func_scores.py''' 
+	print "normalizing selection simulates to neutral: " 
+	print inputScoreFile
+	print neutNormfilename
+	bins, nums, means, variances = read_neut_normfile(neutNormfilename, 'ihs')
+	nsnps = 0
+	normfilename = inputScoreFile + ".norm"	
+	openfile = open(writefilename, 'r')
+	normfile = open(normfilename, 'w')
+	for line in openfile:
+		entries = line.split()
+		freq_allele1 = float(entries[2]) #1 is ancestral, 0 is derived in tped
+ 		unnormed_ihs_val = float(entries[5]) #locus           phys-pos        1_freq          ihh_1           ihh_0           ihs             derived_ihh_left     derived_ihh_right    ancestral_ihh_left      ancestral_ihh_right
+		for ibin in range(len(bin_bounds)):
+			if freq_allele1 <= bin_bounds[ibin]:
+				normalizedvalue = (unnormed_ihs_val - means_bin[ibin])/sqrt(vars_bin[ibin])
+				break
+		writeline = line.strip('\n') +"\t" + str(normalizedvalue) + '\n'
+		normfile.write(writeline)		
+	openfile.close()
+	normfile.close()
+	print "wrote to: " + normfilename
+	retur
+def norm_neut_xpehh(inputScoreFile, outfileName, runProgram = "scans.py"):
+	'''from func_clean.py'''
+	cmdStr = "python " + runProgram + " selscan_norm_xpehh " + inputScoreFile + " > " + outfileName
+	print cmdStr
+	return
+def norm_sel_xpehh(inputScoreFile, neutNormfilename, bin_bounds):
+	''' from normalize_Xp_manually in func_scores.py'''
+	num, mean, var = read_neut_normfile(neutNormfilename, scoretype = 'xp')
+	normfilename = inputScoreFile + ".norm"
+	openfile = open(inputScoreFile, 'r')
+	normfile = open(normfilename, 'w')
+	header = openfile.readline()
+	normfile.write(header)
+	#ADJUST EACH VALUE 
+	for line in openfile:
+		entries = line.split()
+		unnormed_xpehh = float(entries[-1])
+		normed_xpehh = (unnormed_xpehh - mean)/sqrt(var)
+		writeline = line.strip('\n') + "\t" + str(normed_xpehh) + "\n"
+		normfile.write(writeline)
+	openfile.close()
+	normfile.close()
+	print "wrote to: " + normfilename
+	retur
+
+##################
+## HISTOGRAMS ###
+###################
+def get_hist_bins(score,numBins):
+	"""toggle cutoffs of histogram, as well as y-axis limits"""
+	if score == "ihs":
+		scorerange = [-4., 4.]
+		ylims = [0, .25]
+	elif score == "delihh":
+		scorerange = [-2., 3.]
+		ylims = [0, .25]
+	elif score == "fst":
+		scorerange = [-.05, 1.]#[-2., 4.5]
+		ylims = [0, .25]
+	elif score == "deldaf":
+		scorerange = [-1., 1.]
+		ylims = [0, .25]
+	elif score == "xp":
+		scorerange = [-3., 7.]
+		ylims = [0, .25]
+	binlen = (scorerange[1] - scorerange[0])/float(numBins-1)
+	bins = [scorerange[0] + binlen * i for i in range(numBins)]
+	return bins, scorerange, ylims
+def get_indices(score, dem_scenario):
+	"""CRUCIAL FUNC; tells loadVals which columns to grab and return; changes with filetype"""
+	if score == "ihs":
+		physpos_index, freq_anc_index = 1, 2
+		if "sel" in dem_scenario:
+			ihs_unnormed_index, ihs_normed_index, expectedlen = 9, 10, 11 #these files have 11 columns
+		else: #neutral
+			ihs_unnormed_index, ihs_normed_index, expectedlen = 5, 6, 8 #these files have 8 columns; last one is binary variable
+		indices = [physpos_index, ihs_normed_index]#freq_anc_index, ihs_unnormed_index, ihs_normed_index] 
+	elif score == "delihh":
+		expectedlen = 8
+		physpos_index, freq_anc_index = 1, 2
+		delihh_unnormed_index, delihh_normed_index = 5, 6
+		indices = [physpos_index, delihh_normed_index]#freq_anc_index, delihh_unnormed_index, delihh_normed_index]
+	elif score == "fst":
+		expectedlen = 2
+		physpos_index, scoreindex = 0, 1
+		indices = [physpos_index, scoreindex]
+	elif score == "deldaf":
+		expectedlen = 2
+		physpos_index, scoreindex = 0, 1
+		indices = [physpos_index, scoreindex]
+	elif score == "xp":
+		physpos_index, freq_anc_index = 1, 2
+		xp_unnormed_index, xp_normed_index, = 7, 8
+		if "sel" in dem_scenario:
+			expectedlen = 9
+		else:
+			expectedlen = 10
+		indices = [physpos_index, xp_normed_index]
+		#[physpos_index, freq_anc_index, xp_unnormed_index, xp_normed_index]
+	return expectedlen, indices
+def load_vals_from_files(filename, numCols, takeindices, stripHeader = False):
+	''' from func_clean.py '''
+	toreturn, incompleteData = [[] for index in takeindices], 0
+	openfile = open(filename, 'r')
+	if stripHeader:
+		header = openfile.readline()
+	for line in openfile:
+		entries = line.split()
+		if len(entries) != numCols:
+			print "ERROR: numCols " + str(numCols) + " " + str(len(entries)) + " " + filename
+			incompleteData +=1
+		for iIndex in range(len(takeindices)):
+			index = takeindices[iIndex]
+			thisValue = float(entries[index])
+			toreturn[iIndex].append(thisValue)
+	openfile.close()
+	return toreturn
--- cms/likes_from_model.py
+++ cms/likes_from_model.py
@@ -4,7 +4,7 @@
 prefixstring = "{CMS2.0}>>\t\t" #for stderr (make global?)
 from dists.func import get_bins, check_make_dir, give_command, check_bin_filled
 import argparse
-import sys
+import sys, os
 
 #############################
 ## DEFINE ARGUMENT PARSER ###
@@ -27,6 +27,7 @@ def full_parser_likes_from_model():
 	
 	run_sel_sims_parser = subparsers.add_parser('run_sel_sims', help='run sel. simulations')	
 	run_sel_sims_parser.add_argument('n', action='store', type=int, help='num replicates to run per sel scenario')
+	run_sel_sims_parser.add_argument('trajDir', action='store', type=int, help='location of simulated trajectories (i.e. outputDir from get_sel_trajs)')
 	
 	##########################
 	### COSI - SHARED ARGS  ##
@@ -37,29 +38,45 @@ def full_parser_likes_from_model():
 		cosi_parser.add_argument('--cosiBuild', action='store', help='which version of cosi to run? (*automate installation)', default="/Users/vitti/Desktop/COSI_DEBUG_TEST/cosi-2.0/coalescent")
 		cosi_parser.add_argument('--dropSings', action='store', type=float, help='randomly thin global singletons from output dataset to model ascertainment bias')
 
-	##############
-	## SEL BINS ##
-	##############
-	for sel_parser in [get_sel_trajs_parser, run_sel_sims_parser]:
-		sel_parser.add_argument('--freqRange', type=str, help="range of final selected allele frequencies to simulate, e.g. .05-.95", default='.05-.95')
-		sel_parser.add_argument('--nBins', type=int, help="number of frequency bins", default=9)
-	
 	################################
-	## CALCULATE SCORES FROM SIMS ##
+	## CALCULATE SCORES FROM SIMS ## I'm not sure if we even want to include this. It just wraps infrastructure that already exists elsewhere. leave as exercise for the user?
 	################################
 	scores_from_sims_parser = subparsers.add_parser('scores_from_sims', help='get scores from simulations')
-	scores_from_sims_parser.add_argument('inputTpedDir', help='directory containing all simulated tpeds from which to calculate')
-	scores_from_sims_parser.add_argument('outputScoreDir', help='directory to write output score files')		
-	scores_from_sims_parser.add_argument('--ihs', action='store_true')	#includes norm? MAYBE: run ihs is one thing (user specifies neut/sel), norm is another thing, norm_sel is another thing
-	scores_from_sims_parser.add_argument('--normalizeIhs', action='store_true')	
+	#PER POP:
+	scores_from_sims_parser.add_argument('inputTped', help='tped from which to calculate score')
+	scores_from_sims_parser.add_argument('outputFilename', help='where to write scorefile')		
+	scores_from_sims_parser.add_argument('--ihs', action='store_true')
+	scores_from_sims_parser.add_argument('--delIhh', action='store_true')
+	#POP COMPARISONS:
+	scores_from_sims_parser.add_argument('--xpehh', action='store', help="inputTped for altpop")
+	scores_from_sims_parser.add_argument('--fst_deldaf', action='store', help="inputTped for altpop")
+	#NORMALIZE: 
+	scores_from_sims_parser.add_argument('--normalizeIhs', action='store', help="filename for parameters to normalize to; if not given then will by default normalize file to its own global dist")	
+	scores_from_sims_parser.add_argument('--normalizeDelIhh', action='store', help="filename for parameters to normalize to; if not given then will by default normalize file to its own global dist")
+	scores_from_sims_parser.add_argument('--normalizeXpehh', action='store', help="filename for parameters to normalize to; if not given then will by default normalize file to its own global dist")
+
 
 	##################################################
 	## GATHER SCORES AND CALCULATE LIKELIHOOD TABLE ##
 	##################################################
-
 	likes_from_scores_parser = subparsers.add_parser('likes_from_scores', help='get component score probability distributions from scores')
 	likes_from_scores_parser.add_argument('--write', action='store_true', help='once scores have been calculated and normalized, bin values and write probability distributions to file')
 	likes_from_scores_parser.add_argument('--plot', action='store_true', help='once probability distributions written to file, visualize')	
+	likes_from_scores_parser.add_argument('neutFile', action='store', help='file with scores for neutral scenarios (normalized if necessary)')
+	likes_from_scores_parser.add_argument('selFile', action='store', help='')#THIS GONNA HAVE TO BE A DIRECTORY for all the selfreqs? #file with scores for selected scenarios (normalized if necessary)')
+	likes_from_scores_parser.add_argument('selPos', action='store', type=int, help='position of causal variant', default=500000)
+	likes_from_scores_parser.add_argument('--ihs', action='store_true', help='')	
+	likes_from_scores_parser.add_argument('--delihh', action='store_true', help='')	
+	likes_from_scores_parser.add_argument('--xp', action='store_true', help='')	
+	likes_from_scores_parser.add_argument('--deldaf', action='store_true', help='')	
+	likes_from_scores_parser.add_argument('--fst', action='store_true', help='')	
+
+	##############
+	## SEL BINS ##
+	##############
+	for sel_parser in [get_sel_trajs_parser, run_sel_sims_parser, likes_from_scores_parser]:
+		sel_parser.add_argument('--freqRange', type=str, help="range of final selected allele frequencies to simulate, e.g. .05-.95", default='.05-.95')
+		sel_parser.add_argument('--nBins', type=int, help="number of frequency bins", default=9)
 	return parser
 
 #####################
@@ -85,11 +102,12 @@ def execute_run_neut_sims(args):
 def execute_get_sel_trajs(args):
 	'''adapted from JV run_sims_from_model_vers.py'''
 	selTrajDir = args.outputDir
-	if args.outputDir[-1] != "/":
+	if selTrajDir[-1] != "/":
 		selTrajDir += "/"
+	selTrajDir += "sel_trajs/"
 	runDir = check_make_dir(selTrajDir)
 	fullrange, bin_starts, bin_ends, bin_medians, bin_medians_str = get_bins(args.freqRange, args.nBins)
-	print prefixstring + "running " + str(args.nSimsPerBin) + " selection simulates per for each of " + str(args.nBins) + " frequency bins, using model: \n\t\t" + args.inputParamFile
+	print prefixstring + "running " + str(args.nSimsPerBin) + " selection trajectories per for each of " + str(args.nBins) + " frequency bins, using model: \n\t\t" + args.inputParamFile
 	print prefixstring + "outputting to " + runDir
 	selSimCommand_base = args.cosiBuild + " -p " + args.inputParamFile +  " --genmapRandomRegions --output-gen-map " #allow user to pass these?
 	if args.dropSings is not None:
@@ -113,19 +131,108 @@ def execute_get_sel_trajs(args):
 				unfilled = False
 	return
 def execute_run_sel_sims(args):
-	print prefixstring + "running " + str(args.n) + " sel simulates per scenario from model: " + args.inputParamFile
+	'''after get_sel_trajs has been run, use trajectories to generate simulated tped data.'''
+	selSimDir = args.outputDir
+	if selSimDir[-1] != "/":
+		selSimDir += "/"
+	selSimDir += "sel_sims/"
+	runDir = check_make_dir(selSimDir)
+	print prefixstring + "loading trajectories from " + args.trajDir
+	print prefixstring + "outputting to " + runDir
+	fullrange, bin_starts, bin_ends, bin_medians, bin_medians_str = get_bins(args.freqRange, args.nBins)
+	for ibin in range(args.nBins):
+		trajDir = runDir + "seltraj_" + bin_medians_str[ibin]
+		trajectoryFilenames = os.listdir(trajDir)
+		print "JV: loaded " + str(len(trajectoryfilenames)) + " files ..."
+		print "JV: how best to parallelize this step? use same infrastructure as execute_run_neut?"
 	return
 def execute_scores_from_sims(args):
-	''' adapted from JV scores_from_tped_vers.py '''
-	inputs = os.listdir(args.inputTpedDir)
-	print prefixstring + "calculating from " + str(len(inputs)) + " simulates: " + args.inputTpedDir
-	print prefixstring + "writing scores to: " + args.outputDir
-	if args.neutIhs is not None:
-		inputs = os.listdir(args.inputTpedDir)
-
+	''' adapted from JV scores_from_tped_vers.py. points to scans.py '''
+	#inputs = os.listdir(args.inputTpedDir)
+	#print prefixstring + "calculating from " + str(len(inputs)) + " simulates: " + args.inputTpedDir
+	#print prefixstring + "writing scores to: " + args.outputScoreDir
+	inputTped, outputFilename = args.inputTped, args.outputFilename
+	if args.ihs:
+		calc_ihs(inputTped, outputFilename)
+		#for inputTped in inputs:
+			#outfilename = args.outputScoreDir + "_ihs"
+			#calc_ihs(inputTped, outfilename) #parallelize? 
+	if args.delIhh:
+		#get ihs file!
+		print "get concatenated ihs file..."
+		ihsfilename = ""
+		calc_delihh(ihsfilename, outputFilename)
+	if args.xpehh is not None:
+		altinputTped = args.xpehh
+		calc_xpehh(inputTped, altinputTped, outputFilename)		
+	if args.fst_deldaf is not None:
+		altinputTped = args.fst_deldaf
+		calc_fst_deldaf(inputTped, altinputTped, outputFilename)
+	if args.normalizeIhs is not None:
+		#if the normargfile exists, use it. otherwise, just pipe to scans.py
+		pass
+	if args.normalizeDelIhh is not None:
+		pass
+	if args.normalizeXpehh is not None:
 		pass
 	return
 def execute_likes_from_scores(args):
+	'''adapted from likes_from_scores_vers.py'''
+	fullrange, bin_starts, bin_ends, bin_medians, bin_medians_str = get_bins(args.freqRange, args.nBins)
+	data = {}
+	if args.ihs:
+		comparison, stripHeader = False, False
+		expectedlen_neut, indices_sel = get_indices(args.neutFile, "ihs_neut")
+		expectedlen_sel, indices_sel = get_indices(args.selFile, "ihs_sel")
+	if args.delihh:
+		comparison, stripHeader = False, False
+		expectedlen_neut, indices_sel = get_indices(args.neutFile, "delihh_neut")
+		expectedlen_sel, indices_sel = get_indices(args.selFile, "delihh_sel")
+		pass
+	if args.xp:
+		comparison, stripHeader = True, True
+		expectedlen_neut, indices_sel = get_indices(args.neutFile, "xp_neut")
+		expectedlen_sel, indices_sel = get_indices(args.selFile, "xp_sel")
+	if args.deldaf:
+		comparison, stripHeader = True, False
+		expectedlen_neut, indices_sel = get_indices(args.neutFile, "deldaf_neut")
+		expectedlen_sel, indices_sel = get_indices(args.selFile, "deldaf_sel")
+	if args.fst:
+		comparison, stripHeader = True, False
+		expectedlen_neut, indices_sel = get_indices(args.neutFile, "fst_neut")
+		expectedlen_sel, indices_sel = get_indices(args.selFile, "fst_sel")
+		pass
+				#MUST DO THIS FOR EACH FREQBIN
+		# OR DO I? CAN I ASSUME USER HAS CONCATENATED IN ORDER TO NORMALIZE...?
+
+	val_array = load_vals_from_files(args.neutFile, expectedlen_neut, indices_neut, stripHeader, verbose)		
+	neut_positions, neut_score_final = val_array[0], val_array[1]
+	val_array = load_vals_from_files(args.selFile, expectedlen_sel, indices_sel, stripHeader, verbose)		
+	sel_positions, sel_score_final = val_array[0], val_array[1]
+	# BINS! 
+
+	causal_indices = [i for i, x in enumerate(sel_positions) if x == args.selPos]
+	linked_indices = [i for i, x in enumerate(sel_positions) if x != args.selPos] 
+
+	causal_positions = [sel_positions[variant] for variant in causal_indices]
+	causal_score_final = [sel_score_final[variant] for variant in causal_indices]
+	
+	linked_positions = [sel_positions[variant] for variant in linked_indices]
+	linked_score_final = [sel_score_final[variant] for variant in linked_indices]
+	datatypes = []
+	for status in ['causal', 'linked', 'neut']:
+		for dpoint in ['positions', 'score_final']:
+			datatypes.append(status + "_" + dpoint)
+	for datatype in datatypes:
+		key = (score, dem_scenario, selpop, altpop, datatype, selfreq) #BINS!
+		data[key] = eval(datatype)
+	#if args.write:
+	#	pass
+	if args.plot:
+		if comparison: #fst, xpehh, deldaf
+			pass
+		else: #ihs, delihh
+			pass
 	print prefixstring + "MUST CONNECT likes_from_model.py to likes_from_scores_vers.py"
 	return
 
