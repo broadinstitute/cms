b9bdad221796c7c16d9bafe3cf8ad1418a97ca92
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-07-12 14:26:45 -0400

    start using conda env file for unit test environment

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -4,22 +4,33 @@ set -e
 # the miniconda directory may exist if it has been restored from cache
 if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
     echo "Miniconda install already present from cache: $MINICONDA_DIR"
+    export PATH="$MINICONDA_DIR/bin:$PATH"
+    hash -r
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     
     if [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-    else
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+            wget https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        else
+            wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+        fi
+     else
+        if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        else
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        fi
     fi
 
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
-    export PATH="$PATH:$MINICONDA_DIR/bin"
+    export PATH="$MINICONDA_DIR/bin:$PATH"
     hash -r
     conda config --set always_yes yes --set changeps1 no
     conda config --add channels bioconda
     conda config --add channels r
     conda update -q conda
-    conda info -a # for debugging
 fi
+
+conda info -a # for debugging
