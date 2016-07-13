44897ff817a6b366da7a30f33b7cb20def8d3ab5
Christopher Tomkins-Tinch <tomkinsc@broadinstitute.org>
2016-07-13 13:59:24 -0400

    consistent environment file extensions -> *.yaml

--- travis/install-environment.sh
+++ travis/install-environment.sh
@@ -2,9 +2,9 @@
 
 if [[ "$(python -c 'import sys; print(sys.version[0])')" == 2* ]]; then
     echo "Creating conda environment using Py2 environment file"
-    conda env create -f conda-environment_py2.yml -p $HOME/cms-env
+    conda env create -f conda-environment_py2.yaml -p $HOME/cms-env
 else
     echo "Creating conda environment using Py3 environment file"
-    conda env create -f conda-environment_py3.yml -p $HOME/cms-env
+    conda env create -f conda-environment_py3.yaml -p $HOME/cms-env
 fi
 
