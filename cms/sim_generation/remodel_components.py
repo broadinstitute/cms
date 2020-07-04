cce6e9fa6ce550a5006a58f24376746cfaabbbd6
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2020-07-03 20:28:38 -0400

    fix file paths (still hard-coded)

--- cms/sim_generation/remodel_components.py
+++ cms/sim_generation/remodel_components.py
@@ -1,8 +1,8 @@
 ##	run new, heterogenous demography model set; generate component scores
 ##	02.21.2019
 
-cmsdir = "/idi/sabeti-scratch/jvitti/cms/cms/"
-writedir = "/idi/sabeti-scratch/jvitti/remodel/run4/"
+cmsdir = "/data/ilya-work/proj/cms2-work/cms/cms/cms/"
+writedir = "/data/mytmp/run5/"
 #"/idi/sabeti-scratch/jvitti/remodel/run2/"
 simRecomFile =  "/idi/sabeti-scratch/jvitti/params/test_recom.recom"
 pops = [1, 2, 3, 4]
@@ -39,7 +39,7 @@ def main():
 		print(('missing: ', tped_filename))	
 		sys.exit(0)				
 
-	ihh12_commandstring = "/idi/sabeti-scratch/jvitti/selscan/bin/linux/selscan"
+	ihh12_commandstring = "/ilya/miniconda3/envs/selscan-env/bin/selscan"
 	#"python " + cmsdir + "scans.py selscan_ihh12" 
 	ihh12_unnormedfileprefix = basedir + "ihh12/" + replicate_idstring2
 	#ihh12_argstring = tped_filename + " " + ihh12_unnormedfileprefix + " --threads 7 "
@@ -53,12 +53,12 @@ def main():
 		execute(ihh12_fullcmd)
 
 
-	ihs_commandstring = "/idi/sabeti-scratch/jvitti/selscan/bin/linux/selscan"
+	ihs_commandstring = "/ilya/miniconda3/envs/selscan-env/bin/selscan"
 	#ihs_commandstring = "python " + cmsdir + "scans.py selscan_ihs"
 	ihs_outfileprefix = basedir + "ihs/" + replicate_idstring2
 	ihs_unnormedfile = ihs_outfileprefix + ".ihs.out"
 	#ihs_argstring = tped_filename + " " + ihs_outfileprefix + " --threads 7 "
-	ihs_argstring = " --ihs --tped " + tped_filename + " --out " + ihs_outfileprefix + " --threads 8"
+	ihs_argstring = " --ihs --ihs-detail --tped " + tped_filename + " --out " + ihs_outfileprefix + " --threads 8"
 	ihs_fullcmd = ihs_commandstring + " " + ihs_argstring
 	#ihs_normedfile = ihs_unnormedfile + ".norm"
 	print(ihs_fullcmd)
@@ -74,7 +74,7 @@ def main():
 	if not os.path.isfile(delihh_unnormedfile) and not os.path.isfile(delihh_unnormedfile + ".gz"):
 		execute(delihh_fullcmd)		
 	
-	nsl_commandstring = "/idi/sabeti-scratch/jvitti/selscan/bin/linux/selscan"
+	nsl_commandstring = "/ilya/miniconda3/envs/selscan-env/bin/selscan"
 	#nsl_commandstring = "python " + cmsdir + "scans.py selscan_nsl" 
 	nsl_unnormedfileprefix = basedir + "nsl/" + replicate_idstring2
 	#nsl_argstring = tped_filename + " " + nsl_unnormedfileprefix
@@ -92,7 +92,7 @@ def main():
 	altpops.remove(int(thispop))
 	for altpop in altpops:
 		#xpehh_commandstring = "python " + cmsdir + "scans.py selscan_xpehh --threads 7"
-		xpehh_commandstring = "/idi/sabeti-scratch/jvitti/selscan/bin/linux/selscan"
+		xpehh_commandstring = "/ilya/miniconda3/envs/selscan-env/bin/selscan"
 		#tped2 = tpeddir + "rep" + str(irep) + "_" + str(altpop) + ".tped"
 		#tped_filename2 = get_tped_filename(selpop, irep, ancestralpop, altpop, model, tped_dir)
 		tped_filename2 = tped_dir + replicate_idstring + "_0_" + str(altpop) + ".tped"
